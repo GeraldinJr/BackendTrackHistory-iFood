9954cf8facd9265e586585919853b7a7d2957e06
Geraldo Júnior <geraldo.junior0207@gmail.com>
2022-03-21 14:47:41 -0300

    commit inicial

---
+++ src/main/java/br/com/bolinhocorp/BackendTrackHistoryiFood/BackendTrackHistoryIFoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.bolinhocorp.BackendTrackHistoryiFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendTrackHistoryIFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendTrackHistoryIFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bolinhocorp/BackendTrackHistoryiFood/BackendTrackHistoryIFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bolinhocorp.BackendTrackHistoryiFood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTrackHistoryIFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
