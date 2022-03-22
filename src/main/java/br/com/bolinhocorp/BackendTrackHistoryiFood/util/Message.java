3b9472ce6055eb5cb038cd759114f007d144285f
Edvan-Jr <edvan.hd@gmail.com>
2022-03-22 13:30:03 -0300

    feat(back): [BJB] creating package util/message

---
+++ src/main/java/br/com/bolinhocorp/BackendTrackHistoryiFood/util/Message.java
@@ -0,0 +1,18 @@
+package br.com.bolinhocorp.BackendTrackHistoryiFood.util;
+
+public class Message {
+	private String message;
+
+	public Message(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
