f1c0464f1e130f8218e6afe431b4a396b4962260
debora.brum <debrumneves@gmail.com>
2022-03-25 18:10:35 -0300

    bugfix(back): [BJB] arrumando o historico para retornar apenas os trackings do pedido selecionado

--- src/main/java/br/com/bolinhocorp/BackendTrackHistoryiFood/dao/TrackHistoryDAO.java
+++ src/main/java/br/com/bolinhocorp/BackendTrackHistoryiFood/dao/TrackHistoryDAO.java
@@ -1,5 +1,7 @@
 package br.com.bolinhocorp.BackendTrackHistoryiFood.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +13,6 @@ public interface TrackHistoryDAO extends CrudRepository<TrackHistory, Integer>{
 
     @Query(value="select * from track_history where pedido_id = ?1 order by instante_registro desc limit 1", nativeQuery = true)
     public TrackHistory recuperarUltimoPeloPedidoId(Integer pedido_id);
+    
+    public List<TrackHistory> findByPedido_id(Integer id);
 }
