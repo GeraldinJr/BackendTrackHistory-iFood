c82d78c6f7f68a0975258742c6f904cda3584c97
Geraldo Júnior <geraldo.junior0207@gmail.com>
2022-03-29 11:32:54 -0300

    Verificação de pedido em rota atribuído a pessoa entregadora implementado

--- src/main/java/br/com/bolinhocorp/BackendTrackHistoryiFood/dao/TrackHistoryDAO.java
+++ src/main/java/br/com/bolinhocorp/BackendTrackHistoryiFood/dao/TrackHistoryDAO.java
@@ -13,6 +13,9 @@ public interface TrackHistoryDAO extends CrudRepository<TrackHistory, Integer>{
 
     @Query(value="select * from track_history where pedido_id = ?1 order by instante_registro desc limit 1", nativeQuery = true)
     public TrackHistory recuperarUltimoPeloPedidoId(Integer pedido_id);
+
+    @Query(value="select * from track_history where pessoa_entregadora_id = ?1 order by instante_registro desc limit 1", nativeQuery = true)
+    public TrackHistory recuperarUltimoPelaPessoaId(Integer pessoa_id);
     
     public List<TrackHistory> findByPedido_id(Integer id);
 }
