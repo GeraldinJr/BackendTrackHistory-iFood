d05176b5ec74c4dda6995d38ab66d85503873007
debora.brum <debrumneves@gmail.com>
2022-03-31 09:54:36 -0300

    bugfix[BJB]: adicionando verificacao no atribuir pedido

--- src/main/java/br/com/bolinhocorp/BackendTrackHistoryiFood/services/ITrackHistoryService.java
+++ src/main/java/br/com/bolinhocorp/BackendTrackHistoryiFood/services/ITrackHistoryService.java
@@ -5,10 +5,9 @@ import java.util.ArrayList;
 import br.com.bolinhocorp.BackendTrackHistoryiFood.dto.DadosGeoMaisInstDTO;
 import br.com.bolinhocorp.BackendTrackHistoryiFood.models.TrackHistory;
 
-import javax.persistence.criteria.CriteriaBuilder;
-
 public interface ITrackHistoryService {
 	public TrackHistory cadastrarTracking(TrackHistory track);
 	public ArrayList<DadosGeoMaisInstDTO> recuperarTodos(Integer id, Integer offset, Integer limit);
 	public TrackHistory recuperarUltimoPeloPedidoId(Integer id);
+	public TrackHistory recuperarUltimoPelaPessoaId(Integer id);
 }
